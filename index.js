53b9dfac0a79d1181af71212809e6dd9bda7ef1e
David Portilla <davidportilla179@hotmail.com>
2021-05-26 22:27:39 -0500

    add structure

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors= require('cors');
+
+const mongoose = require('mongoose');
+
+const app = express();
+
+// Manejando los errores 404
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Iniciando el servidor...
+var server = app.listen(process.env.PORT || 3000, function(){
+  console.log('Escuchando en el puerto ' + server.address().port);
+});
+
+//users - places - 
